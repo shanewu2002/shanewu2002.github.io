88c35b84a5f6ff570063ec7772d370216a654b9d
shanewu2002 <shanewu2002@gmail.com>
2023-01-09 01:42:20 +0800

    final01090142

--- analysis/demo.js
+++ analysis/demo.js
@@ -3,14 +3,12 @@ var menuOpen = false;
 function showHideMenuItems (event) {
 	event.stopPropagation();
 	let menuItems = $('nav > a:not(:first-of-type)');
-	if (menuOpen) {
-		// menuItems.css('display', 'none');
+	if(menuOpen) {
 		menuItems.removeClass('open');
 		$('nav > a > span').removeClass('rotated');
 		menuOpen = false;
 	}
 	else {
-		// menuItems.css('display', 'inline');
 		menuItems.addClass('open');
 		$('nav > a > span').addClass('rotated');
 		menuOpen = true;
@@ -19,7 +17,6 @@ function showHideMenuItems (event) {
 
 function hideMenuItems () {
 	let menuItems = $('nav > a:not(:first-of-type)');
-	// menuItems.css('display', 'none');
 	menuItems.removeClass('open');
 	$('nav > a > span').removeClass('rotated');
 	menuOpen = false;
@@ -28,4 +25,4 @@ function hideMenuItems () {
 window.onload = function () {
 	$('nav > a:first-of-type').on('click', showHideMenuItems);
 	$(document).on('click', hideMenuItems);
-}
+};
